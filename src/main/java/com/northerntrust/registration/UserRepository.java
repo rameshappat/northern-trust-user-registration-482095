ca1a4bb45607b1af567fd699bc18c808b90dd9c0
Ramesh Appat <rameshappat@gmail.com>
2025-12-25 16:31:28 -0700

    feat: add UserRepository.java

---
+++ src/main/java/com/northerntrust/registration/UserRepository.java
@@ -0,0 +1,7 @@
+package com.northerntrust.registration;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    // Custom query methods can be defined here
+}
